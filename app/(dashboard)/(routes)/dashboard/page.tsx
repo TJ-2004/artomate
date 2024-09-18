2ce47d48cadf044f0844823ee14f00c9d7785686
TJ-2004 <124814940+TJ-2004@users.noreply.github.com>
2024-09-19 01:19:54 +0530

    Add SideBar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,13 +1,9 @@
-import { Button } from "@/components/ui/button";
-import { SignedIn, UserButton } from "@clerk/nextjs";
+
 
 const DashboardPage = () => {
   return (
     <div>
       <p>DashboardPage(Protected)</p>
-      <SignedIn>
-          <UserButton />
-        </SignedIn>
     </div>
   );
 };

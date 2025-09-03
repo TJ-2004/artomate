4ef093b8595be5ff041af5c28d76df70c46f27c8
TJ-2004 <124814940+TJ-2004@users.noreply.github.com>
2025-09-03 12:10:50 +0530

    vercel fix 2

--- app/api/music/route.ts
+++ app/api/music/route.ts
@@ -7,7 +7,7 @@ const replicate = new Replicate({
 });
 export async function POST(req: Request) {
   try {
-    const { userId } = auth();
+    const { userId } = await auth();
     const body = await req.json();
     const { prompt } = body;
     console.log(prompt);
@@ -18,7 +18,7 @@ export async function POST(req: Request) {
       return new NextResponse("Unauthorized", { status: 401 });
     }
     if (!prompt) {
-      return new NextResponse("Prompt are required", { status: 400 });
+      return new NextResponse("Prompt is required", { status: 400 });
     }
     const response = await replicate.run(
       "riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05",

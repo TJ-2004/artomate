230f6caeb9e67d3a4f2a0622f50aa930d1504564
TJ-2004 <124814940+TJ-2004@users.noreply.github.com>
2025-09-06 18:09:28 +0530

    ✅remove settings

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -64,11 +64,6 @@ const routes = [
     href: "/code",
     color: "text-green-700",
   },
-  {
-    label: "Settings",
-    icon: Settings,
-    href: "/settings",
-  },
 ];
 const Sidebar = ({ closeSidebar }: { closeSidebar?: () => void }) => {
   const pathname = usePathname();
